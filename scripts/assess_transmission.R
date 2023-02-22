ca0c41482fb78ac30e5266c09d0dd29e4e17d15f
JHerbeck-IDM <jherbeck@idmod.org>
2023-02-22 10:54:21 -0800

    Bug in make_new_infecteds()

--- scripts/assess_transmission.R
+++ scripts/assess_transmission.R
@@ -8,7 +8,7 @@ assess_transmission <- function(df1, df2) {
       df2$transmission[i] <- ifelse(
         df2$removal[i] == 0 &&
           # Individuals can't be removed and transmit (i.e. if they == 1)
-          runif(1, min = 0, max = 1) < df1$transmission_risk_per_timestep[i],
+          runif(1, min = 0, max = 1) < df1$transmission_risk_per_day[i],
         1,
         0
       )
--- scripts/make_new_infecteds.R
+++ scripts/make_new_infecteds.R
@@ -11,24 +11,25 @@ make_new_infecteds <- function(n){
   
   df <- data.frame(
     "ID" = seq( from = nrow(population_summary) + 1, to = nrow(population_summary) + n, by = 1),
-    "removal_rate" = rates$removal_rate,
-    "partners" = rates$partners,
-    #"susceptible_partners" = s,
+    "removal_rate" = rates$removal_rate, # per day
+    "partners" = rates$partners, # partners per day
     "acts_per_day" = rates$acts_per_day,
     "transmission_risk_per_act" = rates$lambda,
-    #"acts_per_timestep" = floor(acts_per_day * (timestep * 365) * 1.0), # 1.0 is fraction susceptible
+    
+    #"acts_per_timestep" = floor(rates$acts_per_day * (rates$timestep * 365) * rates$partners)
     # Whatever the timestep is, this "acts_per_day*(timestep*365)" will report out in days
     # Which is necessary because "acts_per_day" is in "days" units (contacts per day)
-    "transmission_risk_per_timestep" = 1 - (1 - rates$lambda) ^
-      floor(rates$acts_per_day * (timestep * 365) * rates$partners),
-    # 1 - (1 - population_summary$transmission_risk_per_act)^(population_summary$infectious_acts_per_timestep)
-    "infection_source" = transmitters,
-    "infection_year" = infection_times,
-    # ADD in transmission rate modifier based on time since infection
-    "sampling_year" = NA,
-    # timestep of removal = 1
-    "cumulative_partners" = NA,
-    "cumulative_transmissions" = NA
+    
+    "transmission_risk_per_day" = 1 - (1 - rates$lambda) ^
+      floor(rates$acts_per_day * rates$partners),
+    
+    # 1 - (1 - population_summary$transmission_risk_per_act)^(population_summary$acts_per_timestep)
+    "infection_source" = 0,
+    "infection_year" = 0,
+    
+    "sampling_year" = 0,
+    "cumulative_partners" = 0,
+    "cumulative_transmissions" = 0
   )
   return(df)
 }
