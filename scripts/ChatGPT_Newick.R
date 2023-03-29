8ef4273e9318495c7e71dd83efe16b05aded8321
JHerbeck-IDM <jherbeck@idmod.org>
2023-03-29 13:22:24 -0700

    Update ChatGPT_Newick.R

--- scripts/ChatGPT_Newick.R
+++ scripts/ChatGPT_Newick.R
@@ -6,9 +6,39 @@ library(igraph)
 # read in transmission line list
 line_list <- read.csv("line_list.csv")
 
+# function to calculate leaf edge length
+calculate_leaf_edge_length <- function(df, recipient_id) {
+  # subset the data frame to rows where the recipient is the specified ID
+  recipient_rows <- df[df$recipient == recipient_id, ]
+  
+  # check if the recipient is also a source in any other rows
+  if (recipient_id %in% recipient_rows$source) {
+    # subset to rows where the recipient is a source
+    recipient_source_rows <- recipient_rows[recipient_rows$source == recipient_id, ]
+    
+    # find the row with the highest infection time
+    highest_infection_time_row <- recipient_source_rows[which.max(recipient_source_rows$infectionTime), ]
+    
+    # calculate the leaf edge length using the infection time from the highest row
+    leaf_edge_length <- highest_infection_time_row$sampleTime - highest_infection_time_row$infectionTime
+  } else {
+    # calculate the leaf edge length using the infection time from the current row
+    leaf_edge_length <- recipient_rows$sampleTime - recipient_rows$infectionTime
+  }
+  
+  # return the leaf edge length
+  return(leaf_edge_length)
+}
+
+# create a new variable "leafEdgeLength" using the helper function
+line_list$leafEdgeLength <- sapply(line_list$recipient, function(recipient_id) calculate_leaf_edge_length(line_list, recipient_id))
+
+# print the resulting data frame
+print(line_list)
+
 
 test <- "((1:2, ((2:1, (3:1,5:1):0.5):1, ((4:2, (6:1, 8:1):1):1, 7:2):1):1):1);"
-test <- "((1:2, ((2:1, (3:1):0.5):1, ((4:2):1, 7:2):1):1):1);"
+test <- ((1:2, ((2:1, (3:1,5:1):0.5):1, ((4:2, (6:1, 8:1):1):1, 7:2):1):1):1);"
 
 tree <- ape::read.tree(text = test)
 plot(tree)
